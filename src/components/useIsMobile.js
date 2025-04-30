5b1d16f6181288c6ac89c2d896825d0136e44c4c
JUGEWEB <gamanshop33@gmail.com>
2025-04-30 18:43:51 +0300

    'update'

--- src/components/useIsMobile.js
+++ src/components/useIsMobile.js
@@ -4,7 +4,8 @@ import { useState, useEffect } from "react";
 function getScreenSize() {
   const width = window.innerWidth;
   return {
-    isVerySmall: width <= 480,
+    isVeryVerySmall: width<345,
+    isVerySmall: width>=345 && width<= 480,
     isSmallMobile: width > 480 && width <= 600,
     isMobile: width > 600 && width < 768,
     isTablet: width >= 768 && width < 1024,
