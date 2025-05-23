6c1fa5572804b54e9ebd93b6fa8d4a9a84be4a38
JUGEWEB <gamanshop33@gmail.com>
2025-05-23 03:05:39 +0300

    'update'

--- src/components/likedItem.js
+++ src/components/likedItem.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import "./LikedItems.css";
+import useScreenSize from "./useIsMobile";
 
 const BASE_URL = "https://api.malidag.com";
 
@@ -11,6 +12,7 @@ const LikedItems = ({ auth }) => {
   const navigate = useNavigate(); // Initialize navigation
   const location = useLocation(); // ✅ Get current route
   const isLikedPage = location.pathname === "/likeditem"; // ✅ Check if on Liked Items page
+  const {isMobile, isDesktop, isSmallMobile, isTablet, isVerySmall} = useScreenSize()
 
   // 🔹 Fetch liked items when component mounts
   useEffect(() => {
@@ -46,23 +48,26 @@ const LikedItems = ({ auth }) => {
   return (
     <div className="liked-container">
       <h2>What you like</h2>
+      <div style={{display: (!(isDesktop || isTablet || isLikedPage)) ? "flex" : "", overflowX: (!(isDesktop || isTablet || isLikedPage)) ? "auto" : "", justifyContent: (!(isDesktop || isTablet || isLikedPage)) ? "start" : ""}}>
       {likedItems.length === 0 ? (
         <p>No liked items yet.</p>
       ) : (
         (isLikedPage ? likedItems : likedItems.slice(0, 5)).map((item) => (
-          <div key={item.id} className="liked-item" style={{ display: isLikedPage ? "flex" : "" }}>
+          <div key={item.id} className="liked-item" style={{ display: isLikedPage ? "flex" : "", padding: "2px" }}>
             {/* Clicking image navigates to product page */}
-            <div style={{ height: "300px", width: "300px", filter: "brightness(0.95)", backgroundColor: "white" }}>
+            <div style={{ height:(!(isDesktop || isTablet || isLikedPage)) ? "100px" : "300px", width:(!(isDesktop || isTablet || isLikedPage)) ? "100px" : "300px", filter: "brightness(0.95)", backgroundColor: "white" }}>
               <img
                 src={item.image}
                 alt={item.name}
                 className="liked-item-image"
                 onClick={() => handleNavigate(item.id)}
+                style={{height:(!(isDesktop || isTablet || isLikedPage)) ? "100px" : "300px"}}
               />
             </div>
 
             {/* Clicking name navigates to product page */}
-            <p className="liked-item-name" onClick={() => handleNavigate(item.id)}>
+            <p className="liked-item-name" style={{maxWidth:(!(isDesktop || isTablet || isLikedPage)) ? "100px" : "300px", maxHeight:(!(isDesktop || isTablet || isLikedPage)) ? "100px" : "300px",  textOverflow: "ellipsis",whiteSpace: "nowrap",
+    overflow: "hidden",  }} onClick={() => handleNavigate(item.id)}>
               {item.name} - ${item.price}
             </p>
 
@@ -75,6 +80,7 @@ const LikedItems = ({ auth }) => {
           </div>
         ))
       )}
+      </div>
     </div>
   );
 };
